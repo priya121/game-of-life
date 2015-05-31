893f442d8e59224b5f33c9b2a38d4da064a90f05
Priya Patil <priya10487@aol.com>
2015-05-31 10:52:33 +0100

    bug in some of the initial tests

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -13,42 +13,45 @@ end
 class World
     attr_reader :cells
 
-    def initialize(cells = [],width,height)
+    def initialize(cells,width,height)
         @cells  = cells
         @width = width
         @height = height
     end
 
     def tick
-        for i in 0..cells.length
-            if cells[i] == 1 || cells[i] == 0
+        new_cells = []
+        for i in 0..@cells.length
+            if @cells[i] == 1 || @cells[i] == 0
                 if neighbour_count(i) == 2 || neighbour_count(i) == 3
-                    cells[i] = 1
+                    new_cells[i] = 1
                 else
-                    cells[i] = 0
+                    new_cells[i] = 0
                 end
             end
         end
+        @cells = new_cells
     end
 
-    def neighbours(index,x,y) 
+    def neighbours(index) 
+        x = index % @width
+        y = (index / @width).to_i
         n = []
         n << index - @width - 1 if x > 0 && y > 0 
         n << index - @width if y > 0 
         n << index - @width + 1 if x < @width - 1 && y > 0 
         n << index - 1  if x > 0  
         n << index + 1 if x < @width - 1 
-        n << index + @width - 1 if x > 0 || y < @height - 1
+        n << index + @width - 1 if x > 0 && y < @height - 1
         n << index + @width if y < @height - 1
         n << index + @width + 1 if x < @width - 1 && y < @height - 1 
-
-        return n
+        n
     end
 
     def neighbour_count(cell)
         count = 0  
         neighbours(cell).each do |index|
-            count += cells[index] 
+            count += @cells[index] 
         end
         return count
     end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,83 +1,78 @@
 require "game_of_life"
 
 describe "game of life" do 
-    xit "creates an empty world" do 
-        empty_world = World.new
-        expect(empty_world.cells).to eq([])
-    end 
-
-    xit "takes a 2d game with no live cells and returns an empty world" do 
-        game = World.new([0,0,0,0])
+    it "takes a 2d game with no live cells and returns an empty world" do 
+        game = World.new([0,0,0,0],2,2)
         game.tick
         expect(game.cells).to eq([0,0,0,0])
     end
 
-    xit "takes a 2d game with one live cell and returns an empty world" do 
-        game = World.new([1,0,0,0])
+    it "takes a 2d game with one live cell and returns an empty world" do 
+        game = World.new([1,0,0,0],2,2)
         game.tick
         expect(game.cells).to eq([0,0,0,0])
     end
 
-    xit "takes a 2d game with two live cells and returns an empty world" do 
-        game = World.new([1,1,0,0])
+    it "takes a 2d game with two live cells and returns an empty world" do 
+        game = World.new([1,1,0,0],2,2)
         game.tick
         expect(game.cells).to eq([0,0,0,0])
     end
 
-    xit "takes a 2d game with one dead cell and returns four live cells" do 
-        game = World.new([1,1,1,0])
+    it "takes a 2d game with one dead cell and returns four live cells" do 
+        game = World.new([1,1,1,0],2,2)
         game.tick
         expect(game.cells).to eq([1,1,1,1])
     end
 
-    xit "takes a 2d game with 4 live cells and all stay alive in the next generation" do 
-        game = World.new([1,1,1,1])
+    it "takes a 2d game with 4 live cells and all stay alive in the next generation" do 
+        game = World.new([1,1,1,1],2,2)
         game.tick
         expect(game.cells).to eq([1,1,1,1])
     end
 
-    xit "takes a 3d game with no live cells and returns an empty world" do
-        game = World.new([0,0,0,0,0,0,0,0,0])
+    it "takes a 3 game with no live cells and returns an empty world" do
+        game = World.new([0,0,0,0,0,0,0,0,0],3,3)
         game.tick
         expect(game.cells).to eq([0,0,0,0,0,0,0,0,0])
     end
 
-    xit "takes a 9 cell game with one live cell and returns an empty world" do 
-        game = World.new([1,0,0,0,0,0,0,0,0])
+    it "takes a 9 cell game with one live cell and returns an empty world" do 
+        game = World.new([1,0,0,0,0,0,0,0,0],3,3)
         game.tick
         expect(game.cells).to eq([0,0,0,0,0,0,0,0,0])
     end
 
-    xit "takes a 9 cell game with two live cells and returns an empty world" do
-        game = World.new([1,1,0,0,0,0,0,0,0])
+    it "takes a 9 cell game with two live cells and returns an empty world" do
+        game = World.new([1,1,0,0,0,0,0,0,0],3,3)
         game.tick
         expect(game.cells).to eq([0,0,0,0,0,0,0,0,0])
     end
 
-    xit "takes a 9 cell game with three live cells and returns four live cells and three dead cells" do
-        game = World.new([1,1,1,0,0,0,0,0,0])
+    it "takes a 9 cell game with three live cells and returns four live cells and three dead cells" do
+        game = World.new([1,1,1,0,0,0,0,0,0],3,3)
         game.tick
-        expect(game.cells).to eq([0,1,0,1,1,0,0,0,0])
+        expect(game.cells).to eq([0,1,0,0,1,0,0,0,0])
     end
 
     it "takes a 5 by 5 board and for a cell with index returns neighbours indicies" do
         game = World.new([],5,5)
-        expect(game.neighbours(7,2,2)).to eq([1,2,3,6,8,11,12,13])
-        expect(game.neighbours(12,2,2)).to eq([6,7,8,11,13,16,17,18])
-        #expect(game.neighbours(0,0,0)).to eq([1,5,6])
-        expect(game.neighbours(4,4,0)).to eq([3,8,9])
-        expect(game.neighbours(3,2,0)).to eq([2,4,7,8,9])
-        expect(game.neighbours(17,2,3)).to eq([11,12,13,16,18,21,22,23])
+        expect(game.neighbours(7)).to eq([1,2,3,6,8,11,12,13])
+        expect(game.neighbours(12)).to eq([6,7,8,11,13,16,17,18])
+        expect(game.neighbours(0)).to eq([1,5,6])
+        expect(game.neighbours(4)).to eq([3,8,9])
+        expect(game.neighbours(3)).to eq([2,4,7,8,9])
+        expect(game.neighbours(17)).to eq([11,12,13,16,18,21,22,23])
     end
     
     it "takes a 6 by 6 board and for a given cell with index returns neighbours indicies" do 
         game = World.new([],6,6)
-        expect(game.neighbours(1,1,0)).to eq([0,2,6,7,8])
+        expect(game.neighbours(1)).to eq([0,2,6,7,8])
     end
 
     it "takes a 7 by 7 board and for a given cell with an index returns neighbours indicies" do 
         game = World.new([],7,7)
-        expect(game.neighbours(13,6,1)).to eq([5,6,12,19,20])
+        expect(game.neighbours(13)).to eq([5,6,12,19,20])
     end
     
 end
